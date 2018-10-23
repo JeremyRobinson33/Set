2120d6a6223ddac83e4db16b2ea105da5099a275
Jeremy Robinson <jrobinson7@pride.hofstra.edu>
2018-10-23 17:58:09 -0400

    almost finished

---
+++ Set/SetCardButtons.swift
@@ -0,0 +1,9 @@
+//
+//  SetCardButtons.swift
+//  Set
+//
+//  Created by Jeremy Robinson on 9/29/18.
+//  Copyright © 2018 Jeremy Robinson. All rights reserved.
+//
+
+import Foundation
---
+++ Set/SetGame.swift
@@ -0,0 +1,9 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Jeremy Robinson on 9/27/18.
+//  Copyright © 2018 Jeremy Robinson. All rights reserved.
+//
+
+import Foundation
